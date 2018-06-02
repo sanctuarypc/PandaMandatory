ac08c63d4c7c03e9215c870d7c8e0a1ead8dd721
Mohamed <mtraore01@icloud.com>
2018-06-02 19:10:38 +0200

    Add first file

---
+++ PandaBot/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  PandaBot
+//
+//  Created by PandaMachine on 02/06/2018.
+//  Copyright © 2018 PandaMachine. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
